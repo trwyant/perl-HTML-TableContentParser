0e86edbc69174b6ac358f2e3244bafd52b369839
Tom Wyant <wyant@cpan.org>
2021-01-06 10:55:04 -0500

    Version 0.302_01
    Set version to 0.302_01. Update Changes file for changes since v00.302.

--- lib/HTML/TableContentParser.pm
+++ lib/HTML/TableContentParser.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use parent qw{ HTML::Parser };
 
-our $VERSION = '0.302';
+our $VERSION = '0.302_01';
 
 our $CLASSIC = 0;
 our $DEBUG = 0;
@@ -415,7 +415,7 @@ Thomas R. Wyant, III F<wyant at cpan dot org>
 
 Copyright (C) 2002 Simon Drabble
 
-Copyright (C) 2017-2020 Thomas R. Wyant, III
+Copyright (C) 2017-2021 Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
